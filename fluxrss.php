9091eec9fc506c9e1dbef3ef0e6ccdbae3e61d8a
mambax7 <mambax7@gmail.com>
2021-08-12 22:44:10 -0400

    strict types declaration

--- fluxrss.php
+++ fluxrss.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
 -------------------------------------------------------------------------
                      ADSLIGHT 2 : Module for Xoops
--- language/english/mail_template/index.php
+++ language/english/mail_template/index.php
@@ -1,3 +1,5 @@
 <?php
 
+declare(strict_types=1);
+
 header('HTTP/1.0 404 Not Found');
